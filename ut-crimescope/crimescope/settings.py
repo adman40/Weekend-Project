e3010b28750628b03d849446f9061f5609130f51
adman40 <adman40@cs.utexas.edu>
2026-02-28 19:04:57 -0600

    Initial Django Scaffolding

--- ut-crimescope/crimescope/settings.py
+++ ut-crimescope/crimescope/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'crimescope.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'crimescope',
+         'USER': 'crimescope_user',
+         'PASSWORD': 'Bevo123',
+         'HOST': 'localhost',
+         'PORT': '5432',
     }
 }
 
